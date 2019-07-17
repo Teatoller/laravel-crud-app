c5a909b6611623b57808e54152975ba6421c6e2f
Teatoller <steven.otieno1@gmail.com>
2019-07-17 22:37:41 +0300

    (update-controller): implement update in controller
    - add update method in controller
    
    [Starts #014]

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -89,6 +89,17 @@ class ShareController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $request->validate([
+            'share_name'=>'required',
+            'share_price'=>'required|integer',
+            'share_qty'=>'required|integer'
+        ]);
+        $share = Share::find($id);
+        $share->share_name = $request->get('share_name');
+        $share->share_price = $request->get('share_price');
+        $share->share_qty = $request->get('share_qty');
+        $share->save();
+        return redirect('/shares')->with('success', 'Stock has been updated');
     }
 
     /**
